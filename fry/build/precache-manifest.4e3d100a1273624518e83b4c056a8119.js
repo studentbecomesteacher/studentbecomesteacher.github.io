33b3fe5886f0b48ee764447e25a92080a66ba704
studentbecomesteacher <86862524+studentbecomesteacher@users.noreply.github.com>
2021-07-05 14:26:47 +0100

    Add files via upload

---
+++ fry/build/precache-manifest.4e3d100a1273624518e83b4c056a8119.js
@@ -0,0 +1,286 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3f36867fd520e8da266ed5da76d1a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60ddbd66ed19139bd525",
+    "url": "/static/css/3.0371faf3.chunk.css"
+  },
+  {
+    "revision": "300e27c5e54a03b613cf",
+    "url": "/static/css/main.02703514.chunk.css"
+  },
+  {
+    "revision": "51ba6b82e7f23fcbfaa9",
+    "url": "/static/js/0.2fe5fc95.chunk.js"
+  },
+  {
+    "revision": "0c40326d3a83de984d57",
+    "url": "/static/js/10.04af183d.chunk.js"
+  },
+  {
+    "revision": "6c642681dd359c99ff93",
+    "url": "/static/js/11.aca10f9a.chunk.js"
+  },
+  {
+    "revision": "60ddbd66ed19139bd525",
+    "url": "/static/js/3.0898bd00.chunk.js"
+  },
+  {
+    "revision": "e6e3a897522402c362e54b8f819616df",
+    "url": "/static/js/3.0898bd00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1343223b7cb6785bec6d",
+    "url": "/static/js/4.dddc1f34.chunk.js"
+  },
+  {
+    "revision": "5f1256e6ec10135f6451",
+    "url": "/static/js/5.e3136ebb.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/static/js/5.e3136ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94fa4b903e58494512eb",
+    "url": "/static/js/6.fcb83cb3.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/6.fcb83cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8fd9739a0fb4908b5fb",
+    "url": "/static/js/7.f564509f.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/7.f564509f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052bc412b2559bbe2962",
+    "url": "/static/js/8.eba30d24.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.eba30d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e206a6f546d5e942062",
+    "url": "/static/js/9.12bcbbf0.chunk.js"
+  },
+  {
+    "revision": "300e27c5e54a03b613cf",
+    "url": "/static/js/main.a8ffa9da.chunk.js"
+  },
+  {
+    "revision": "9e94d6e618a83febbc97",
+    "url": "/static/js/runtime-main.2b099c27.js"
+  },
+  {
+    "revision": "b28b70cc762f8721a28e3a5127b7149b",
+    "url": "/static/media/ada-bnb.b28b70cc.png"
+  },
+  {
+    "revision": "44c5947934450419a08273f2c6c7c3b9",
+    "url": "/static/media/ada-bnbv2.44c59479.svg"
+  },
+  {
+    "revision": "08f3293725a10ab158ffbbc3a604370a",
+    "url": "/static/media/alpha-bnbv2.08f32937.svg"
+  },
+  {
+    "revision": "dd16e7305e9765cbfa12583f053ebddb",
+    "url": "/static/media/atom-bnbv2.dd16e730.svg"
+  },
+  {
+    "revision": "85cc63dd610046d03cede7e66d0a1dcd",
+    "url": "/static/media/band-bnbv2.85cc63dd.svg"
+  },
+  {
+    "revision": "856e6d2fdfcfc14b6ff86125468ae243",
+    "url": "/static/media/bch-bnbv2.856e6d2f.svg"
+  },
+  {
+    "revision": "95f6970670d6272de327cdb0e030d995",
+    "url": "/static/media/blk-bnb.95f69706.svg"
+  },
+  {
+    "revision": "a2879d28cecb14b6e04ad1d52ef4c190",
+    "url": "/static/media/btcb-bnb.a2879d28.png"
+  },
+  {
+    "revision": "4e485a1c5734a2a9be51ac59dab5dfcc",
+    "url": "/static/media/btcb-bnbv2.4e485a1c.png"
+  },
+  {
+    "revision": "ae692cccbf19181a3f092a7dc99c2927",
+    "url": "/static/media/busd-bnb.ae692ccc.png"
+  },
+  {
+    "revision": "75aa2672cc65211d69822e843ad25f83",
+    "url": "/static/media/busd-bnbv2.75aa2672.png"
+  },
+  {
+    "revision": "b886e630e21ef53fa8a49343f7e09ecc",
+    "url": "/static/media/cake-bnb.b886e630.png"
+  },
+  {
+    "revision": "40b81a7762d3cff35c191ceb4f2ee706",
+    "url": "/static/media/cake-bnbv2.40b81a77.png"
+  },
+  {
+    "revision": "bba1cc02264278d46ac1e813be33a3a2",
+    "url": "/static/media/chef.bba1cc02.png"
+  },
+  {
+    "revision": "4fd8091566cf04aeb152a8214ad085f9",
+    "url": "/static/media/combo1.4fd80915.png"
+  },
+  {
+    "revision": "328153c947cac0a7c9b5537a7a7b541a",
+    "url": "/static/media/ctk-bnb.328153c9.png"
+  },
+  {
+    "revision": "7b8820f62dbc0defd2ed51746b091520",
+    "url": "/static/media/ctk-bnbv2.7b8820f6.svg"
+  },
+  {
+    "revision": "ad5e93096ee84aedf8d9051af01de47f",
+    "url": "/static/media/dai-bnbv2.ad5e9309.svg"
+  },
+  {
+    "revision": "87f731c557814fa93a8e68c34b6d6fb9",
+    "url": "/static/media/dot-bnbv2.87f731c5.svg"
+  },
+  {
+    "revision": "f98fc28bca5f0404847463394b09164f",
+    "url": "/static/media/eos-bnbv2.f98fc28b.svg"
+  },
+  {
+    "revision": "326715e6f7558d7158359f16022f6ecf",
+    "url": "/static/media/eth-bnb.326715e6.png"
+  },
+  {
+    "revision": "9ec6d3a25ffd7e9fab6f96f1038888cc",
+    "url": "/static/media/eth-bnbv2.9ec6d3a2.png"
+  },
+  {
+    "revision": "f959962b2481ab5d1cd308c381674787",
+    "url": "/static/media/fil-bnb.f959962b.png"
+  },
+  {
+    "revision": "73d9b0a2ab8891a685699fcb3cdc3429",
+    "url": "/static/media/fil-bnbv2.73d9b0a2.svg"
+  },
+  {
+    "revision": "4fc1b0aa1cc533a4399a1c041fc53176",
+    "url": "/static/media/friesandketchup.4fc1b0aa.png"
+  },
+  {
+    "revision": "f48db7d68f10184299f4be72daa3fa46",
+    "url": "/static/media/fryreaper.f48db7d6.png"
+  },
+  {
+    "revision": "60d4824ea314efeec879e0211a511134",
+    "url": "/static/media/inj-bnb.60d4824e.png"
+  },
+  {
+    "revision": "e6f5f5a45f1fc229568d418bd5f95bbe",
+    "url": "/static/media/inj-bnbv2.e6f5f5a4.svg"
+  },
+  {
+    "revision": "74cb612fde783434ea22d4a710685032",
+    "url": "/static/media/link-bnbv2.74cb612f.svg"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "765c4d875919c63385e628d95535f214",
+    "url": "/static/media/reaper-bake.765c4d87.svg"
+  },
+  {
+    "revision": "78127e9c8d71b971f76cec35e59d1797",
+    "url": "/static/media/stax-cake.78127e9c.png"
+  },
+  {
+    "revision": "aec3e30563101a26b674e58f313c7f60",
+    "url": "/static/media/stax-cakev2.aec3e305.png"
+  },
+  {
+    "revision": "11cea30337d0ae72c2084bbf233486fa",
+    "url": "/static/media/sxp-bnb.11cea303.png"
+  },
+  {
+    "revision": "48f91388d313ee345b611545c16c0cd2",
+    "url": "/static/media/sxp-bnbv2.48f91388.svg"
+  },
+  {
+    "revision": "a887264a6011230e0ff3a9ec9e15f689",
+    "url": "/static/media/twt-bnbv2.a887264a.svg"
+  },
+  {
+    "revision": "3544a5525583aa600ad46dc54d5ac882",
+    "url": "/static/media/uni-bnb.3544a552.png"
+  },
+  {
+    "revision": "20d3a50529d33d0fa660341949a870d2",
+    "url": "/static/media/uni-bnbv2.20d3a505.png"
+  },
+  {
+    "revision": "36736288bb4a84fa1aa1a28c49478620",
+    "url": "/static/media/usdc-bnb.36736288.png"
+  },
+  {
+    "revision": "ce0696d2994324b69f1ba0e6053573c1",
+    "url": "/static/media/usdc-bnbv2.ce0696d2.svg"
+  },
+  {
+    "revision": "6d749afe6148be616fee7117f72b3562",
+    "url": "/static/media/usdt-bnbv2.6d749afe.svg"
+  },
+  {
+    "revision": "7c59d647c682c468de81eed34e26388e",
+    "url": "/static/media/usdt-busd.7c59d647.png"
+  },
+  {
+    "revision": "bcfa52f149a87de85233420e39d35c44",
+    "url": "/static/media/usdt-busdv2.bcfa52f1.png"
+  },
+  {
+    "revision": "d62f9ca60e6cc3ea856d2abe826eef1d",
+    "url": "/static/media/vai-busdv2.d62f9ca6.png"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/static/media/wallet-connect.37f2bc6e.svg"
+  },
+  {
+    "revision": "efac3d3956da430697d4a0b5d0f1a691",
+    "url": "/static/media/xrp-bnbv2.efac3d39.svg"
+  },
+  {
+    "revision": "b6cd78fed246b540f2553e0e4ec40cc5",
+    "url": "/static/media/xtz-bnbv2.b6cd78fe.svg"
+  },
+  {
+    "revision": "06f8698ac5b84181ed6567342f634ee5",
+    "url": "/static/media/xvs-bnbv2.06f8698a.svg"
+  },
+  {
+    "revision": "0c1bc0ea6f288b9a5b04b6a713bdea12",
+    "url": "/static/media/yfi-bnb.0c1bc0ea.png"
+  },
+  {
+    "revision": "d49a85b510d04877278e20d6fd951449",
+    "url": "/static/media/yfi-bnbv2.d49a85b5.svg"
+  },
+  {
+    "revision": "f104bfa1d5f22824e1eb54b1aa56015e",
+    "url": "/static/media/yfii-bnbv2.f104bfa1.svg"
+  }
+]);
